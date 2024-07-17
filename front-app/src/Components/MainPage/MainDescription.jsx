09ee189bb97645df3b10f303404071d2eb383a6f
DarkWoodHunter <aserrods@gmail.com>
2024-07-17 20:36:22 -0300

    minor update

--- front-app/src/Components/MainPage/MainDescription.jsx
+++ front-app/src/Components/MainPage/MainDescription.jsx
@@ -1,8 +1,17 @@
-export default function MDescription () {
+export default function MDescription() {
     return (
         <>
             <div className=" bg-slate-900 w-[100%] h-svh">
-
+                <div>
+                    <p>Le damos vida a tus proyectos.</p>
+                    <p>Si lo puedes imaginar, nosotros lo creamos.</p>
+                </div>
+                <div>
+                    <img src=""/>
+                    <img src=""/>
+                    <img src=""/>
+                    <img src=""/>
+                </div>
             </div>
         </>
     )
