0ebe8ebfda7c753d7bd71edb82af139a7391bcb1
DarkWoodHunter <aserrods@gmail.com>
2024-07-26 17:59:37 -0300

    minor css fix

--- front-app/src/Components/TopBar.jsx
+++ front-app/src/Components/TopBar.jsx
@@ -17,7 +17,7 @@ export default function TopBar() {
         <>
             <div className=" w-full h-[150px] bg-blue-900 flex justify-between fixed top-0 drop-shadow-lg">
                 <Link to="/">
-                    <img src={NoniLogo} alt="Noni_log" className=" h-[100px] w-[75px] my-auto mx-[10%] mt-[25px] ml-[200px] mx-[10%] hover:cursor-pointer" />
+                    <img src={NoniLogo} alt="Noni_log" className=" h-[100px] w-[75px] my-auto mx-[10%] mt-[25px] ml-[200px] hover:cursor-pointer" />
                 </Link>
                 <div className="flex mr-[50px]">
                     <TopMenuButton botonName="Inicio" />
